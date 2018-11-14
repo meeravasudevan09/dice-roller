2208b2b5143b89704e63676306ddf9dfa6cfe792
Meera Vasudevan <meera.vasudevan@rea-group.com>
2018-11-14 12:35:18 +1100

    Modify button

--- app/src/main/java/com/example/meeravasudevan/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/meeravasudevan/diceroller/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.meeravasudevan.diceroller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.roll_button)
+        rollButton.text = "Let's Roll"
     }
 }
