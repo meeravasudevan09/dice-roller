1084356a066ac6ba1d966dd2b4617e68efe6f464
Meera Vasudevan <meera.vasudevan@rea-group.com>
2018-11-14 13:12:36 +1100

    Display the apporpriate dice image
    - according to the random number generated

--- app/src/main/java/com/example/meeravasudevan/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/meeravasudevan/diceroller/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.meeravasudevan.diceroller
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,15 +13,24 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.text = "Let's Roll"
-        rollButton.setOnClickListener{
+        rollButton.setOnClickListener {
             rollDice()
         }
     }
 
     private fun rollDice() {
-        val resultText: TextView = findViewById(R.id.result_text)
         val randomInt = (1..6).random()
+        val diceImage: ImageView = findViewById(R.id.dice_image)
 
-        resultText.text = randomInt.toString()
+        val drawableResource = when (randomInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+
+        diceImage.setImageResource(drawableResource)
     }
 }
