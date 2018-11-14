66a29d21c005c4daa43007b19b16404009b72efd
Meera Vasudevan <meera.vasudevan@rea-group.com>
2018-11-14 12:46:48 +1100

    Create function rollDice(), display text onClick button

--- app/src/main/java/com/example/meeravasudevan/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/meeravasudevan/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.meeravasudevan.diceroller
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -14,7 +15,12 @@ class MainActivity : AppCompatActivity() {
         val rollButton: Button = findViewById(R.id.roll_button)
         rollButton.text = "Let's Roll"
         rollButton.setOnClickListener{
-            Toast.makeText(this, "Button clicked", Toast.LENGTH_SHORT).show()
+            rollDice()
         }
+    }
+
+    private fun rollDice() {
+        val resultText: TextView = findViewById(R.id.result_text)
+        resultText.text = "Dice rolled"
     }
 }
